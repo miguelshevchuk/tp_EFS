63bb83b7e4834ec40adb813ddd2163262eec3d4f
Migue <shevchuk.miguel@gmail.com>
2021-09-26 00:13:18 -0300

    docker

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,9 @@ echo "Generando nueva imagen"
 sudo docker build . -t efs/node-web-app
 echo "Eliminando contenedor viejo"
 docker stop efs
+docker stop bd_sisop
 docker rm efs
 echo "Desplegando nuevo contenedor"
+docker start bd_sisop
 docker run --name efs -p 7680:7680 efs/node-web-app
 exit
